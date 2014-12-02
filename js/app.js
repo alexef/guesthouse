dde85fef0a8d3c698532da5fc8d85f2774e94a88
Alex Eftimie <alex@eftimie.ro>
2014-12-02 18:05:52 +0100

    test director

--- js/app.js
+++ js/app.js
@@ -1,4 +1,20 @@
 require('js/jquery-2.1.1.min');
 require('js/slideshow');
 require('js/handlebars.runtime-v1.3.0');
+require('js/director.min');
 require('build/templates');
+
+$(function() {
+        alert('here');
+
+        function home() {
+            alert('home');
+        }
+
+        var routes = {
+            '/home': home
+        };
+
+        var router = new Router(routes);
+        router.init();
+});
