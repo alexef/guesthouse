6063f6f7c3fc783cc45ab0725b6c44102cfc2f38
Alex Eftimie <alex@eftimie.ro>
2014-12-02 20:22:14 +0100

    Add default routes and route config to app.js. Remove old index.html features.

--- js/app.js
+++ js/app.js
@@ -4,17 +4,44 @@ require('js/handlebars.runtime-v1.3.0');
 require('js/director.min');
 require('build/templates');
 
-$(function() {
-        alert('here');
+$(function () {
+    function homeRoute() {
+        var homeHTML = Templates["templates/home.hbs"]();
+        $("#outlet").html(homeHTML);
 
-        function home() {
-            alert('home');
+        var images = ["Carousel1.png", "Carousel2.png", "Carousel3.png"];
+        var slideshow = new Slideshow('#search', images);
+        $('body').on("click", '.left-a', function () {
+            slideshow.replaceLeft()
+        });
+        $('body').on("click", '.right-a', function () {
+            slideshow.replaceRight()
+        });
+        slideshow.start();
+    }
+
+    var routes = {
+        '/': homeRoute
+        // TODO: add routes for login and register
+    };
+
+    var router = new Router(routes).configure({html5history: true});
+
+    $(window).on("popstate", function (e) {
+        if (e.originalEvent.state !== null) {
+            router.setRoute(location.href);
         }
+    });
 
-        var routes = {
-            '/home': home
-        };
+    $(document).on("click", "a, area", function () {
+        var href = $(this).attr("href");
 
-        var router = new Router(routes);
-        router.init();
+        if (href.indexOf(document.domain) > -1 || href.indexOf(':') === -1) {
+            history.pushState({}, '', href);
+            router.setRoute(href);
+            return false;
+        }
+    });
+
+    router.init();
 });
